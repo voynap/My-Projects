bc514cfbc550bd8e9e79ac304f2eb3f6188c737f
voynap <115977701+voynap@users.noreply.github.com>
2023-02-08 16:09:18 +0600

    Delete WeatherDataCollector directory

--- WeatherDataCollector/src/main/java/voynap/springcourse/FirstRestApp/repositories/PeopleRepository.java
+++
@@ -1,9 +0,0 @@
-package voynap.springcourse.FirstRestApp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import voynap.springcourse.FirstRestApp.models.Person;
-
-@Repository
-public interface PeopleRepository extends JpaRepository<Person, Integer> {
-}
